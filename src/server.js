bb872441f1ecf01f57e07bf6942454b81302cd52
MINHTQ3-VD\MinhTQ3 <minhtq3@rikkeisoft.com>
2023-03-16 11:36:42 +0700

    Config babeljs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { mapOrder } from "./utilities/sorts.js";
+import { mapOrder } from "*/utilities/sorts.js";
+
 const app = express();
 
 const hostname = "localhost";
