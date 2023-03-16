2da1feeb0de71aa222d58f4543dedc29108e3ecb
MINHTQ3-VD\MinhTQ3 <minhtq3@rikkeisoft.com>
2023-03-16 11:15:46 +0700

    Init project Nodejs

---
+++ README.md
@@ -0,0 +1,1 @@
+# Node.js + Expressjs + Mongodb - Building an API app like Trello from scratch
---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { mapOrder } from "./utilities/sorts.js";
+const app = express();
+
+const hostname = "localhost";
+
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.end("<h1>Hello world!</h1><hr/>");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Hello, I'm running at ${hostname}:${port}`);
+});
